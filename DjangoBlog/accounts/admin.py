14705e05eeebb6798b0bb26d94ffce61e1452201
huangqinpingV1 <huangqinping4@gmai.com>
2023-02-08 17:06:39 +0800

    admin修改

--- DjangoBlog/accounts/admin.py
+++ DjangoBlog/accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import BlogUser
-from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 #注册模型
-admin.site.register(BlogUser,UserAdmin)
+admin.site.register(BlogUser)
 

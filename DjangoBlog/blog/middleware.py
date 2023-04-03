2324098a31e0b3482d06a92c233667627017d1ae
huangqinpingV1 <huangqinping4@gmai.com>
2023-04-03 15:23:28 +0800

    自定义中间件获取访问者ip和ua等

--- DjangoBlog/blog/middleware.py
+++ DjangoBlog/blog/middleware.py
@@ -2,34 +2,28 @@
 #ecoding:utf-8
 #自定义中间件
 import time
-from ipware.ip import get_real_ip
-from django.core.cache import cache
+from ipware import get_client_ip
+from user_agents import parse
+
 
 class OnlineMiddleware(object):
-    def process_request(self,request):
-        self.start_time = time.time()
-    
-    def process_view(self,request,view_func,view_args,view_kwargs):
-        """
-        处理当前在线认数
-        """
-        http_user_agent = request.META.get('HTTP_USER_AGENT',[])
-        if 'Spider' in http_user_agent or 'spider' in http_user_agent:
-            return 
-        online_ips = cache.get("online_ips",[])
+    def __init__(self,get_response= None):
+        self.get_response = get_response
+        print("__init__() enter")
+        super().__init__()
 
-        if online_ips:
-            online_ips = cache.get_many(online_ips).keys()
-
-        ip = get_real_ip(request) 
-        cache.set(ip,0,5*60)
-
-        if ip not in online_ips:
-            oline_ips.append(ip)
-        
-        cache.set("online_lips",online_ips)
-
-   def process_response(self,request,response):
-       cast_time = time.time() -self.start_time
-       response.content = response.replace(b'<!!LOAD_TIMES!!>',str.encode(str(cast_time)[:5]))
-       return response
+    def __call__(self,request):
+        """page render time"""
+        start_time = time.time()
+        response =self.get_response(request)
+        http_user_agent = request.META.get('HTTP_USER_AGENT','')
+        ip,_ =get_client_ip(request)
+        user_agent = parse(http_user_agent)
+        print("ua  ="+str(user_agent)+",ip = "+str(ip))
+        if not response.streaming:
+            try:
+                cast_time =time.time() -start_time
+                response.content = response.content.replace(b'<!!LOAD_TIMES!!>',str.encode(str(cast_time)[:5]))
+            except Exception as e:
+                print("Error OnlineMiddleware:%s" % e)
+        return response
